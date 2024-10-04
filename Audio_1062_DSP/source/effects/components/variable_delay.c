6fce881c3349dead3ccc8ae81aea337141dab1c5
Isaac Clark <6147765+irclark2000@users.noreply.github.com>
2024-10-04 01:10:59 -0400

    Add copyright information

--- Audio_1062_DSP/source/effects/components/variable_delay.c
+++ Audio_1062_DSP/source/effects/components/variable_delay.c
@@ -4,6 +4,20 @@
  *  Created on: Sep 21, 2024
  *      Author: isaac
  */
+/*
+Copyright 2024 Isaac R. Clark, Jr.
+
+Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation
+files (the “Software”), to deal in the Software without restriction, including without limitation the rights to use, copy,
+modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software
+is furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+THE SOFTWARE IS PROVIDED “AS IS”, WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES
+OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS
+BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF
+OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+*/
 
 #include <components/variable_delay.h>
 
@@ -19,6 +33,8 @@ void initialize_variable_delay (VARDELAY * vDelay, float *buf, float buf_size, f
 
 /*
  *  gets a delayed sample and puts the input into the buffer.
+ *  uses linear interpolation to fetch non integer delays
+ *  consider using more advanced interpolation
  *  adds feedback if any
  */
 
