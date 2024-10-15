7b054b467e5bc4eade47afc297a48dc596a78c0e
isaac <isaac@isaac-ThinkPad-E470>
2024-10-15 05:50:56 -0400

    Fix typo in AT_NONCACHEABLE_SECTION_ALIGN macro

--- Audio_1062_DSP/source/compatibility_macros/compatibility.h
+++ Audio_1062_DSP/source/compatibility_macros/compatibility.h
@@ -12,7 +12,7 @@
 #define __NOINIT(bank)
 #endif
 #ifndef AT_NONCACHEABLE_SECTION_ALIGN
-//#define AT_NONCACHEABLE_SECTION_ALIGN(x, y) x
+#define AT_NONCACHEABLE_SECTION_ALIGN(x,y) x
 #endif
 
 #endif /* COMPATIBILITY_MACROS_COMPATIBILITY_H_ */
