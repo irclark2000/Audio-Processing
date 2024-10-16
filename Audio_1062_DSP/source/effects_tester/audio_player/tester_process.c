9b174aaba45c89d89d2db081e5bebe50b99687ae
isaac <isaac@isaac-ThinkPad-E470>
2024-10-16 14:06:29 -0400

    add data format parameter to processHalf function

--- Audio_1062_DSP/source/effects_tester/audio_player/tester_process.c
+++ Audio_1062_DSP/source/effects_tester/audio_player/tester_process.c
@@ -7,6 +7,10 @@
 
 #if AUDIO_EFFECTS_TESTER
 #include "tester_process.h"
+static const float S16_TO_FLOAT = 1.0f / 32768.0f; // supported
+static const float S24_TO_FLOAT = 1.0f / (8388608.0f);
+static const float S32_TO_FLOAT = 1.0f / (2147483648.0f);
+static const float F32_TO_FLOAT = 1.0f;
 
 void initializeEffects(float sampleRate) {
 
@@ -14,11 +18,13 @@ void initializeEffects(float sampleRate) {
 
 void processHalf(const void *bufferIn, void *bufferOut,
 		uint32_t frameCount, AUDIOFORMAT audioFmt,
-		float sampleRate) {
+		float sampleRate, AUDIO_DATA_TYPE dataFmt) {
 	static float leftIn, rightIn;
 	static float leftOut, rightOut;
 	const float *bufIn;
 	float *bufOut;
+
+	if (dataFmt != S16) return;
 
 	uint8_t channelCount;
 	uint32_t loopCount;
@@ -35,12 +41,13 @@ void processHalf(const void *bufferIn, void *bufferOut,
 	bufOut = (float*) bufferOut;
 
 	for (int i = 0; i < loopCount; i += channelCount) {
-		leftIn = bufIn[i];
+		leftIn = bufIn[i] * S16_TO_FLOAT;
+		// convert S16 data to float and deal with MONO/STEREO
 		if (audioFmt == MONO) {
 			rightIn = leftIn;
 		}
 		else {
-			rightIn = bufIn[i + 1];
+			rightIn = bufIn[i + 1] * S16_TO_FLOAT;
 		}
 		// no effect code
 		leftOut = leftIn;
@@ -48,8 +55,8 @@ void processHalf(const void *bufferIn, void *bufferOut,
 
 
 
-		bufOut[i] = leftOut;
-		bufOut[i+1] = rightOut;
+		bufOut[i] = leftOut * 32768.0f;
+		bufOut[i+1] = rightOut * 32768.0f;
 	}
 
 }
--- Audio_1062_DSP/source/effects_tester/audio_player/tester_process.h
+++ Audio_1062_DSP/source/effects_tester/audio_player/tester_process.h
@@ -25,9 +25,18 @@ typedef enum {
 	UNKNOWN
 } AUDIOFORMAT;
 
+typedef enum {
+	Unknown = 0,
+	U8, //  | 8-bit unsigned integer                 | [0, 255]                 |
+	S16, // | 16-bit signed integer                  | [-32768, 32767]           |
+	S24, // | 24-bit signed integer (tightly packed) | [-8388608, 8388607]       |
+	S32, // | 32-bit signed integer                  | [-2147483648, 2147483647] |
+	F32  // | 32-bit floating point                  | [-1, 1]                   |
+} AUDIO_DATA_TYPE;
+
 void processHalf(const void *bufferIn, void *bufferOut,
 		uint32_t frameCount, AUDIOFORMAT audioFmt,
-		float sampleRate);
+		float sampleRate, AUDIO_DATA_TYPE dataFmt);
 void initializeEffects(float sampleRate);
 void test_PROCESS (uint32_t update_counter);
 
