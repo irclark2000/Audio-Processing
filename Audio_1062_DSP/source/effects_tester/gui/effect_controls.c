7e340494016acd8a45982257f48247bcd8a7e341
isaac <isaac@isaac-ThinkPad-E470>
2025-01-29 14:08:58 -0500

    setupSliders calculate count inside for loop

--- Audio_1062_DSP/source/effects_tester/gui/effect_controls.c
+++ Audio_1062_DSP/source/effects_tester/gui/effect_controls.c
@@ -141,10 +141,11 @@ static void setupSlidersComponent(DISPLAY_STATE *gui, EFFECT_PARAMS *parameter,
 
 void setupSliders(DISPLAY_STATE *gui, EFFECT_COMPONENT *component,
 		uint8_t channel) {
-	uint8_t count = gui->slider_count[channel];
-	SLIDER_VALUES *slider = &(gui->sliders[channel][count]);
 
 	for (int i = 0; i < component->parameterCount; i++) {
+		uint8_t count = gui->slider_count[channel];
+		SLIDER_VALUES *slider = &(gui->sliders[channel][count]);
+
 		char *name = component->strParameters[i];
 		if (component->strTypes[i][0] == 'S') {
 			slider->control_type = SLIDER;
